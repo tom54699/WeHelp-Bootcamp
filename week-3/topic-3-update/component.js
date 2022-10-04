e506715746d47e4d8c40c74c8db6cf9f3918c2b2
Tom <tom54699@gmail.com>
2022-10-04 15:33:59 +0800

    First Commit

--- week-3/topic-3-update/component.js
+++ week-3/topic-3-update/component.js
@@ -36,13 +36,10 @@ getdata().then(data=>{
     promotionpic[1].appendChild(image_2)
 
     // 註冊
-    let pic_node=document.querySelectorAll(".pic")
-    let title_node=document.querySelectorAll(".title")
+    let pic_node=document.getElementsByClassName("pic")
+    let title_node=document.getElementsByClassName("title")
     // 建立後面8張
-    for(let i=2;i<10;i++){
-        pic_node=document.querySelectorAll(".pic")
-        title_node=document.querySelectorAll(".title")
-
+    for(let i=0;i<8;i++){
         let image=document.createElement("img")
         let text=document.createElement("div")
 
@@ -50,14 +47,14 @@ getdata().then(data=>{
         image.setAttribute("src",`${img_url}`)
         text.textContent=fetch_stitle[i]
 
-        pic_node[i-2].appendChild(image)
-        title_node[i-2].appendChild(text)
+        pic_node[i].appendChild(image)
+        title_node[i].appendChild(text)
     }
     // 希望可以自動產生後面8個圖文
     // 註冊
     let blockbox_node=document.querySelector(".blockbox")
     let static_block_node=document.querySelectorAll(".block")
-    let star_node=document.querySelectorAll(".star")
+    let star_node=document.getElementsByClassName("star")
     function gerneratediv(){
         static_block_node=document.querySelectorAll(".block")
         console.log(static_block_node)
@@ -68,23 +65,22 @@ getdata().then(data=>{
                 block.setAttribute("class","block")
                 blockbox_node.appendChild(block)
                 let block_node=document.querySelectorAll(".block")
-                console.log(block_node)
 
                 let pic=document.createElement("div")
                 pic.setAttribute("class","pic")
                 block_node[i].appendChild(pic)
-                pic_node=document.querySelectorAll(".pic")
+                
 
                 let title=document.createElement("div")
                 title.setAttribute("class","title")
                 block_node[i].appendChild(title)
-                title_node=document.querySelectorAll(".title")
+                
 
                 let star=document.createElement("div")
                 star.setAttribute("class","star")
                 block_node[i].appendChild(star)
-                star_node=document.querySelectorAll(".star")
-
+                
+                
                 let image=document.createElement("img")
                 let image_star=document.createElement("img")
                 let text=document.createElement("div")
@@ -95,6 +91,7 @@ getdata().then(data=>{
                 image_star.setAttribute("src","1142010.png")
                 text.textContent=fetch_stitle[i]
 
+                console.log(pic_node)
                 pic_node[i].appendChild(image)
                 star_node[i].appendChild(image_star)
                 title_node[i].appendChild(text)
@@ -109,6 +106,5 @@ getdata().then(data=>{
     let load = document.getElementById("load")
     load.addEventListener("click",gerneratediv)
 })
-
 
 
