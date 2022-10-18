8b99e8bf96108514e7a9845ca95a22e5e7fc8e5e
Tom <tom54699@gmail.com>
2022-10-18 13:00:30 +0800

    額外作業

---
+++ week-5/data_1.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '?函?蝺刻?',
+  `name` varchar(255) NOT NULL COMMENT '憪?',
+  `username` varchar(255) NOT NULL COMMENT '撣單?迂',
+  `password` varchar(255) NOT NULL COMMENT '撣單撖Ⅳ',
+  `follower_count` int unsigned NOT NULL DEFAULT '0' COMMENT '餈質馱???,
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '閮餃???',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'test2','test','test',999,'2022-10-17 08:55:42'),(2,'Jack','jack01','123',24,'2022-10-17 08:55:42'),(3,'Cathy','cathy0598','456',56,'2022-10-17 08:55:42'),(4,'Robin','robin6891','789',35,'2022-10-17 08:55:42'),(5,'Seil','seil999','0000',101,'2022-10-17 08:55:42'),(6,'擳犖?桃?','puwo123','123',788,'2022-10-17 09:07:06'),(7,'摮急?蝛?,'wookon123','123',99999,'2022-10-17 09:07:37'),(8,'瘥?','bik4568','888',1225,'2022-10-17 09:09:32');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '?函?蝺刻?',
+  `member_id` bigint NOT NULL COMMENT '?????⊥???,
+  `content` varchar(255) NOT NULL COMMENT '???批捆',
+  `like_count` int unsigned NOT NULL DEFAULT '0' COMMENT '???賊?',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '????',
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,1,'雿平敹怠???',99,'2022-10-17 10:12:07'),(2,1,'蝑?銝???擗?,20,'2022-10-17 10:15:29'),(3,1,'?停?臭???!',9,'2022-10-17 10:15:29'),(4,6,'??瘥皛???,1,'2022-10-17 10:15:38'),(5,7,'頞?鞈賭?鈭箄?頨?!!',99999,'2022-10-17 10:15:38'),(6,8,'擳疵?捏??,6666,'2022-10-17 10:15:38');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_like`
+--
+
+DROP TABLE IF EXISTS `post_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_like` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '?函?蝺刻?',
+  `content_id` bigint NOT NULL COMMENT '??蝺刻?',
+  `like_member_id` bigint NOT NULL COMMENT '?????⊥???,
+  PRIMARY KEY (`id`),
+  KEY `like_member_id` (`like_member_id`),
+  KEY `content_id` (`content_id`),
+  CONSTRAINT `post_like_ibfk_1` FOREIGN KEY (`like_member_id`) REFERENCES `member` (`id`),
+  CONSTRAINT `post_like_ibfk_2` FOREIGN KEY (`content_id`) REFERENCES `message` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_like`
+--
+
+LOCK TABLES `post_like` WRITE;
+/*!40000 ALTER TABLE `post_like` DISABLE KEYS */;
+INSERT INTO `post_like` VALUES (1,1,2),(2,1,5),(3,1,6),(4,1,8),(5,2,2),(6,2,7),(7,3,1),(8,4,4),(9,5,1),(10,5,2),(11,5,3),(12,5,4),(13,6,7),(14,6,8);
+/*!40000 ALTER TABLE `post_like` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-18 12:57:57
