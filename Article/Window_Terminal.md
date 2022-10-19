810180884c8756ecefccb13ade1d22ed7db66cab
Tom <tom54699@gmail.com>
2022-10-19 09:45:33 +0800

    First Commit

---
+++ Article/Window_Terminal.md
@@ -0,0 +1,6 @@
+# Window Terminal 下載安裝簡單三步驟  
+## 前言  
+---
+為了更像一個工程師，所以第一次使用終端機來操作MySQL。整次使用下來發現原生的Terminal很陽春，但是我不相信各位神人大大都用這個原生終端機。  
+所以我就上網找到了，微軟有出一個Window Terminal，這次本篇教學是以新手取向，用最簡單快速的步驟安裝設定好Window Terminal，如果要更加深入歡迎搜尋網路上大神的分享~
+## 一，打開Window 內建的 Microsoft Store  
--- week-5/data_1.sql
+++ week-5/data_1.sql
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `member`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `member` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '?函?蝺刻?',
-  `name` varchar(255) NOT NULL COMMENT '憪?',
-  `username` varchar(255) NOT NULL COMMENT '撣單?迂',
-  `password` varchar(255) NOT NULL COMMENT '撣單撖Ⅳ',
-  `follower_count` int unsigned NOT NULL DEFAULT '0' COMMENT '餈質馱???,
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '閮餃???',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `name` varchar(255) NOT NULL COMMENT '姓名',
+  `username` varchar(255) NOT NULL COMMENT '帳戶名稱',
+  `password` varchar(255) NOT NULL COMMENT '帳戶密碼',
+  `follower_count` int unsigned NOT NULL DEFAULT '0' COMMENT '追蹤者數量',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '註冊時間',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -39,7 +39,7 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES (1,'test2','test','test',999,'2022-10-17 08:55:42'),(2,'Jack','jack01','123',24,'2022-10-17 08:55:42'),(3,'Cathy','cathy0598','456',56,'2022-10-17 08:55:42'),(4,'Robin','robin6891','789',35,'2022-10-17 08:55:42'),(5,'Seil','seil999','0000',101,'2022-10-17 08:55:42'),(6,'擳犖?桃?','puwo123','123',788,'2022-10-17 09:07:06'),(7,'摮急?蝛?,'wookon123','123',99999,'2022-10-17 09:07:37'),(8,'瘥?','bik4568','888',1225,'2022-10-17 09:09:32');
+INSERT INTO `member` VALUES (1,'test2','test','test',999,'2022-10-17 08:55:42'),(2,'Jack','jack01','123',24,'2022-10-17 08:55:42'),(3,'Cathy','cathy0598','456',56,'2022-10-17 08:55:42'),(4,'Robin','robin6891','789',35,'2022-10-17 08:55:42'),(5,'Seil','seil999','0000',101,'2022-10-17 08:55:42'),(6,'魔人普烏','puwo123','123',788,'2022-10-17 09:07:06'),(7,'孫悟空','wookon123','123',99999,'2022-10-17 09:07:37'),(8,'比克','bik4568','888',1225,'2022-10-17 09:09:32');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,11 +51,11 @@ DROP TABLE IF EXISTS `message`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `message` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '?函?蝺刻?',
-  `member_id` bigint NOT NULL COMMENT '?????⊥???,
-  `content` varchar(255) NOT NULL COMMENT '???批捆',
-  `like_count` int unsigned NOT NULL DEFAULT '0' COMMENT '???賊?',
-  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '????',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `member_id` bigint NOT NULL COMMENT '留言者會員標號',
+  `content` varchar(255) NOT NULL COMMENT '留言內容',
+  `like_count` int unsigned NOT NULL DEFAULT '0' COMMENT '按讚數量',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '留言時間',
   PRIMARY KEY (`id`),
   KEY `member_id` (`member_id`),
   CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
@@ -68,7 +68,7 @@ CREATE TABLE `message` (
 
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
-INSERT INTO `message` VALUES (1,1,'雿平敹怠???',99,'2022-10-17 10:12:07'),(2,1,'蝑?銝???擗?,20,'2022-10-17 10:15:29'),(3,1,'?停?臭???!',9,'2022-10-17 10:15:29'),(4,6,'??瘥皛???,1,'2022-10-17 10:15:38'),(5,7,'頞?鞈賭?鈭箄?頨?!!',99999,'2022-10-17 10:15:38'),(6,8,'擳疵?捏??,6666,'2022-10-17 10:15:38');
+INSERT INTO `message` VALUES (1,1,'作業快完成了',99,'2022-10-17 10:12:07'),(2,1,'等一下要吃午餐',20,'2022-10-17 10:15:29'),(3,1,'我就是作者拉!',9,'2022-10-17 10:15:29'),(4,6,'我要毀滅世界',1,'2022-10-17 10:15:38'),(5,7,'超級賽亞人變身!!!',99999,'2022-10-17 10:15:38'),(6,8,'魔貫光殺炮',6666,'2022-10-17 10:15:38');
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,9 +80,9 @@ DROP TABLE IF EXISTS `post_like`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `post_like` (
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '?函?蝺刻?',
-  `content_id` bigint NOT NULL COMMENT '??蝺刻?',
-  `like_member_id` bigint NOT NULL COMMENT '?????⊥???,
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
+  `content_id` bigint NOT NULL COMMENT '留言編號',
+  `like_member_id` bigint NOT NULL COMMENT '留言者會員標號',
   PRIMARY KEY (`id`),
   KEY `like_member_id` (`like_member_id`),
   KEY `content_id` (`content_id`),
@@ -110,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-18 12:57:57
+-- Dump completed on 2022-10-19  9:09:37
